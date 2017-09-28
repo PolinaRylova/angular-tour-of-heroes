8d7e22af6326a7648f5677682a0666fdb4054d94
Polina Rylova <mail@polinarya.ru>
2017-09-28 10:51:56 +0500

    Импортировала Router в HeroesComponent, заинжектила Router в приватную переменную конструктора и описала функцию gotoDetail, вызывающую метод роутера navigate()

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
@@ -12,7 +14,9 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[];
   selectedHero: Hero;
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
 
   getHeroes(): void {
     this.heroService.getHeroes().then(heroes => this.heroes = heroes);
@@ -24,5 +28,9 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
