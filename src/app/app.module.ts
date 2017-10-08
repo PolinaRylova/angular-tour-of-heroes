ec475e3de439aa0433a91db72719fd9ec5572e26
Polina Rylova <mail@polinarya.ru>
2017-10-08 18:25:38 +0500

    Установила и подключила in-memory-web-api, создала сервис in-memory-data вместо mock-heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
-import { AppRoutingModule } from './app-routing.module';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
-    DashboardComponent
+    HeroesComponent    
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
