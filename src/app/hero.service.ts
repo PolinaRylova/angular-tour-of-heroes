c3794c59cc8e303c43b21a3dc03d1a3f7b77b464
Polina Rylova <mail@polinarya.ru>
2017-10-08 20:28:57 +0500

    Добавила возможность удалять героя

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -49,4 +49,13 @@ export class HeroService {
       .then(res => res.json() as Hero)
       .catch(this.handleError);
   }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http
+      .delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
 }
